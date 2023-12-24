72ed78d1aa6377f35b74e60147e4291f82cc1679
ivanrj7j <ivanrj7j@gmail.com>
2023-12-24 21:10:01 +0530

    started working on article

--- src/core/models/article.py
+++ src/core/models/article.py
@@ -2,5 +2,24 @@ class Article:
     """
     A model for an article, contains essential details and methods related to an article
     """
-    def __init__(self) -> None:
-        pass
+    def __init__(self, url:str, title:str, content:str, author:str=None, summary:str=None) -> None:
+        self.url = url
+        self.title = title
+        self.content = content
+        self.author = author
+        self.summary = summary
+
+    @classmethod
+    def fromUUID(self, uuid:str):
+        """
+        Fetches an article from the database using the uuid
+        """
+
+        raise NotImplementedError("Method not implemented yet")
+    
+    def getSummary(self):
+        if self.summary is not None and self.summary != "":
+            return self.summary
+        else:
+            raise NotImplementedError("Summarizing text method is not implemented yet")
+        
