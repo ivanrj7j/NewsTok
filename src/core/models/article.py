7ed7c9a80f8c9be457aa9364ebb5623774016765
ivanrj7j <ivanrj7j@gmail.com>
2023-12-24 21:43:15 +0530

    started working on articles

--- src/core/models/article.py
+++ src/core/models/article.py
@@ -1,3 +1,9 @@
+from src.components import client
+from bson import ObjectId
+
+articlesCollection = client["NewsTok"]["articles"]
+# connecting to collection 
+
 class Article:
     """
     A model for an article, contains essential details and methods related to an article
@@ -15,11 +21,35 @@ class Article:
         Fetches an article from the database using the uuid
         """
 
-        raise NotImplementedError("Method not implemented yet")
+        articleData = articlesCollection.find_one({
+            "_id": ObjectId(uuid)
+        })
+
+        if articleData is None:
+            raise ModuleNotFoundError("The given article doesnt exist in the db")
+        
+        return self(articleData["url"], articleData["title"], articleData["content"], articleData["author"], articleData["summary"])
     
+    def register(self):
+        """
+        Registers the article to the database
+        """
+        if articlesCollection.find_one({"url": self.url}) is None:
+            articlesCollection.insert_one({
+                "url": self.url,
+                "title": self.title,
+                "content": self.content,
+                "author": self.author,
+                "summary": self.summary
+            })  
+
     def getSummary(self):
-        if self.summary is not None and self.summary != "":
-            return self.summary
-        else:
+        """
+        Returns the summary of the article, if it does not already exist, 
+        """
+        
+        if self.summary is None or self.summary == "":
             raise NotImplementedError("Summarizing text method is not implemented yet")
+        
+        return self.summary
         
