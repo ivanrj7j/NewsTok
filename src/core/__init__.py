8ee93c231373e1c40503b8273c035341f4fca053
ivanrj7j <ivanrj7j@gmail.com>
2023-12-25 20:45:11 +0530

    finished scheduler

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,3 +1,4 @@
 from src.core.models.article import Article
 from src.core.workers.fetcher import Fetcher
 from src.core.workers.businessinsider import BusinessInsiderFetcher
+from src.core.workers.scheduler import FetcherScheduler
---
+++ src/core/workers/scheduler.py
@@ -0,0 +1,26 @@
+from typing import Type
+from aiohttp import ClientSession
+import asyncio
+
+class FetcherScheduler:
+    """
+    Schedules Fetchers for many urls, calls them async
+    """
+    
+    def __init__(self, fetcher:Type, userAgent:str, urls:list[str]=[]) -> None:
+        self.fetcher = fetcher
+        self.urls = urls
+        self.userAgent = userAgent
+
+    def addURL(self, url:str):
+        self.urls.append(url)
+
+    async def fetch(self):
+        if len(self.urls) > 0:
+            async with ClientSession() as client:
+                jobs = []
+                for url in self.urls:
+                    fetcher = self.fetcher(self.userAgent, url, client)
+                    jobs.append(asyncio.ensure_future(fetcher.getArticle()))
+
+                await asyncio.gather(*jobs)
